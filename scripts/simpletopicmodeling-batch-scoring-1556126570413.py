37e42c1409fa1055e4f955aa479f30b644d24ee7
{1073} Joseph Gibli <Joseph.gibli@ibm.com>
2019-04-24 18:37:50 +0000

    Ensemble model read from dataset and update batch scripts

--- scripts/simpletopicmodeling-batch-scoring-1556126570413.py
+++ scripts/simpletopicmodeling-batch-scoring-1556126570413.py
@@ -14,10 +14,10 @@ from dsx_ml.ml import dsxr_setup_environment
 dsxr_setup_environment()
 
 # define variables
-args = {'target': '/datasets/test-topic-probabilities.csv', 'remoteHost': '', 'sysparm': '', 'livyVersion': 'livyspark2', 'output_datasource_type': '', 'source': '/datasets/test.csv', 'remoteHostImage': '', 'execution_type': 'DSX', 'output_type': 'Localfile'}
+args = {'target': '/datasets/train-topic-probabilities.csv', 'remoteHost': '', 'sysparm': '', 'livyVersion': 'livyspark2', 'output_datasource_type': '', 'source': '/datasets/train.csv', 'remoteHostImage': '', 'execution_type': 'DSX', 'output_type': 'Localfile'}
 input_data = os.getenv("DEF_DSX_DATASOURCE_INPUT_FILE", (os.getenv("DSX_PROJECT_DIR") + args.get("source")))
 output_data = os.getenv("DEF_DSX_DATASOURCE_OUTPUT_FILE", (os.getenv("DSX_PROJECT_DIR") + args.get("target")))
-model_path = os.getenv("DSX_PROJECT_DIR") + os.path.join("/models", os.getenv("DSX_MODEL_NAME","simple-topic-modeling"), os.getenv("DSX_MODEL_VERSION","1"),"model")
+model_path = os.getenv("DSX_PROJECT_DIR") + os.path.join("/models", os.getenv("DSX_MODEL_NAME","simple-topic-modeling"), os.getenv("DSX_MODEL_VERSION","2"),"model")
 
 # load the input data
 dataframe = pd.read_csv(input_data)
@@ -27,8 +27,10 @@ loaded_model = joblib.load(open(model_path, 'rb'))
 
 # predictions
 scoring_result = loaded_model.transform(dataframe)
+
+# save scoring result to given target
 scoring_df = pd.DataFrame(scoring_result)
 scoring_df.columns = ["topic_" + str(i) for i in range(len(scoring_df.columns))]
 
 # save output to csv
-scoring_df.to_csv(output_data, encoding="utf-8", index = False)
+scoring_df.to_csv(output_data, encoding='utf-8', index = False)
