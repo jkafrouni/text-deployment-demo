865bf23f9be9bc423a3565ac4f22a5f373f210ef
{1055} Jerome Kafrouni <jerome.kafrouni@ibm.com>
2019-04-24 13:52:45 +0000

    Modify batch scoring and evaluation scripts to load latest version. Re-run batch scoring

--- scripts/simpletopicmodeling-batch-scoring-1555689018563.py
+++ scripts/simpletopicmodeling-batch-scoring-1555689018563.py
@@ -8,6 +8,8 @@ import dsx_core_utils, re, jaydebeapi
 from sqlalchemy import *
 from sqlalchemy.types import String, Boolean
 
+import json
+
 
 # setup dsxr environmental vars from command line input
 from dsx_ml.ml import dsxr_setup_environment
@@ -17,7 +19,26 @@ dsxr_setup_environment()
 args = {'target': '/datasets/test-topic-probabilities.csv', 'remoteHost': '', 'sysparm': '', 'livyVersion': 'livyspark2', 'output_datasource_type': '', 'source': '/datasets/test.csv', 'remoteHostImage': '', 'execution_type': 'DSX', 'output_type': 'Localfile'}
 input_data = os.getenv("DEF_DSX_DATASOURCE_INPUT_FILE", (os.getenv("DSX_PROJECT_DIR") + args.get("source")))
 output_data = os.getenv("DEF_DSX_DATASOURCE_OUTPUT_FILE", (os.getenv("DSX_PROJECT_DIR") + args.get("target")))
-model_path = os.getenv("DSX_PROJECT_DIR") + os.path.join("/models", os.getenv("DSX_MODEL_NAME","simple-topic-modeling"), os.getenv("DSX_MODEL_VERSION","1"),"model")
+# model_path = os.getenv("DSX_PROJECT_DIR") + os.path.join("/models", os.getenv("DSX_MODEL_NAME","simple-topic-modeling"), os.getenv("DSX_MODEL_VERSION","1"),"model")
+
+# this section was modified to match online scoring scripts style:
+model_name = "simple-topic-modeling"
+version = "latest"
+model_parent_path = os.getenv("DSX_PROJECT_DIR") + os.path.join("/models", os.getenv("DSX_MODEL_NAME", model_name))
+metadata_path = os.path.join(model_parent_path, "metadata.json")
+
+# fetch info from metadata.json
+with open(metadata_path) as data_file:
+  meta_data = json.load(data_file)
+
+# if latest version, find latest version from  metadata.json
+if (version == "latest"):
+  version = meta_data.get("latestModelVersion")
+
+# prepare model path using model name and version
+model_path = os.path.join(model_parent_path, str(version), "model")
+
+# the rest of the script is similar to the regular batch scoring template:
 
 # load the input data
 dataframe = pd.read_csv(input_data)
